0a7c4b33fd8c8899e9a916ff4272e4e3642e955a
cupelapps <cupelapps@gmail.com>
2021-05-05 10:56:39 +0530

    Update cafilterscript.js

--- cafilterscript.js
+++ cafilterscript.js
@@ -1,7 +1,27 @@
 f (typeof(Worker) !== "undefined") {
   // Yes! Web worker support!
   // Some code.....
-  var filterWebWorker = new Worker("cafilterwebworker.js");
+ 
+  var w;
+
+function startWorker() {
+  if(typeof(Worker) !== "undefined") {
+    if(typeof(w) == "undefined") {
+      w = new Worker("cafilterwebworker.js");
+    }
+    w.onmessage = function(event) {
+      //document.getElementById("result").innerHTML = event.data;
+      console.log(event.data);
+    };
+  } else {
+    //document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Workers...";
+  }
+}
+
+function stopWorker() { 
+  w.terminate();
+  w = undefined;
+}
 } else {
   // Sorry! No Web Worker support..
 }
